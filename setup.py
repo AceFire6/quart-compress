4c0dcd15551f21b002d943b82366b4dff2a24ccd
Jethro Muller <JethroMuller@gmail.com>
2020-06-19 11:55:09 +0200

    Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as readme_file:
 
 setuptools.setup(
     name="quart-compress",
-    version="0.1.0",
+    version="0.2.0",
     url="https://github.com/AceFire6/quart-compress",
     license="MIT",
     author="Jethro Muller, William Fagan",
